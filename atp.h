0ef80d7f2cce43c90d98ee4d0283c7931970f6c1
Adhavan <31651104+AdhavanT@users.noreply.github.com>
2020-12-03 00:06:19 +0530

    Cleaned up repo

--- atp.h
+++ atp.h
@@ -5,15 +5,15 @@
 
 /*
 TODO for anyone: This Library doesn't use any C++ features (mostly just syntax) and can easily be ported to C.
-          
+
                  For more info -> github.com/AdhavanT/ATProfiler
 
-Consider adding the "atp_config.atp" file to get the description of and configure macros 
+Consider adding the "atp_config.atp" file to get the description of and configure macros
 
 ATP is a simple-to-use profiler built for games and other real-time applications.
 ATP focuses on low performance overhead and being easy to use.
 The trade-off is building the various "TestType" buffers at program start-up.
-ATP builds a global buffer to hold all "TestType"s 
+ATP builds a global buffer to hold all "TestType"s
  Example:
 
         REGISTER_TEST(timer_name);    //This must be in global scope
@@ -32,7 +32,7 @@ ATP builds a global buffer to hold all "TestType"s
         void do_stuff()
         {
             ATP_BLOCK(timer_name);   //Creates a simple object whose destructor calls ATP_END() at scope end.
-            ..do stuff i guess...    
+            ..do stuff i guess...
         }
 
 */
@@ -50,16 +50,16 @@ ATP builds a global buffer to hold all "TestType"s
 
 
 #ifdef ATP_ONLY_ON_DEBUG
-    #ifndef _DEBUG
-        #define ATP_TURN_OFF
-    #endif
+#ifndef _DEBUG
+#define ATP_TURN_OFF
+#endif
 #endif
 
 
 #ifdef _DEBUG
-    #define ATP_ASSERT(x) if(!(x)) __debugbreak();
+#define ATP_ASSERT(x) if(!(x)) __debugbreak();
 #else
-    #define ATP_ASSERT(X)
+#define ATP_ASSERT(X)
 #endif 
 
 //Dependencies 
@@ -102,9 +102,9 @@ namespace ATP
     TestType* lookup_testtype(const char* name);
 
     //TODO: Agian, do this properly
-    f64 get_ms_from_test(TestType &test);
-    
-    TestType* register_testtype( const char* name_);
+    f64 get_ms_from_test(TestType& test);
+
+    TestType* register_testtype(const char* name_);
 }
 
 #ifndef ATP_TURN_OFF
@@ -113,82 +113,82 @@ namespace ATP
 {
 #ifdef _WIN32 
 
-    #ifdef ATP_USE_QPC
+#ifdef ATP_USE_QPC
 
-        struct ATP_SCOPED_TIMER_BLOCK
+    struct ATP_SCOPED_TIMER_BLOCK
+    {
+        TestType* testtype;
+        LARGE_INTEGER cycles;
+        ATP_SCOPED_TIMER_BLOCK(TestType* tt)
+            :testtype(tt)
         {
-            TestType* testtype;
-            LARGE_INTEGER cycles;
-            ATP_SCOPED_TIMER_BLOCK(TestType* tt)
-                :testtype(tt)
-            {
-                QueryPerformanceCounter(&cycles);
-            }
-            ~ATP_SCOPED_TIMER_BLOCK()
-            {
-                LARGE_INTEGER tmp;
-                QueryPerformanceCounter(&tmp);
-                testtype->info.test_run_cycles = tmp.QuadPart - cycles.QuadPart;
-                testtype->hits++;
-            }
-        };
-    #else 
-        struct ATP_SCOPED_TIMER_BLOCK
+            QueryPerformanceCounter(&cycles);
+        }
+        ~ATP_SCOPED_TIMER_BLOCK()
         {
-            TestType* testtype;
-            uint64 cycles;
-            ATP_SCOPED_TIMER_BLOCK(TestType* tt)
-                :testtype(tt)
-            {
-                cycles = __rdtsc();
-            }
-            ~ATP_SCOPED_TIMER_BLOCK()
-            {
-                testtype->info.test_run_cycles = __rdtsc() - cycles;
-                testtype->hits++;
-            }
-        };
-    #endif
+            LARGE_INTEGER tmp;
+            QueryPerformanceCounter(&tmp);
+            testtype->info.test_run_cycles = tmp.QuadPart - cycles.QuadPart;
+            testtype->hits++;
+        }
+    };
+#else 
+    struct ATP_SCOPED_TIMER_BLOCK
+    {
+        TestType* testtype;
+        uint64 cycles;
+        ATP_SCOPED_TIMER_BLOCK(TestType* tt)
+            :testtype(tt)
+        {
+            cycles = __rdtsc();
+        }
+        ~ATP_SCOPED_TIMER_BLOCK()
+        {
+            testtype->info.test_run_cycles = __rdtsc() - cycles;
+            testtype->hits++;
+        }
+    };
+#endif
 #endif
 }
 
-    #define ATP_REGISTER(name) \
+#define ATP_REGISTER(name) \
      ATP::TestType *name##ATPTEST = ATP::register_testtype(#name)
 
 //Used to call to a test registered in a different compilation unit 
-    #define ATP_EXTERN_REGISTER(name) \
+#define ATP_EXTERN_REGISTER(name) \
      extern ATP::TestType *name##ATPTEST;
 
-    #ifdef ATP_USE_QPC
-        #define ATP_START(name) \
+#ifdef ATP_USE_QPC
+#define ATP_START(name) \
         LARGE_INTEGER ATP_CYCLES_##name;\
         QueryPerformanceCounter(&ATP_CYCLES_##name)
 
-        #define ATP_END(name) \
+#define ATP_END(name) \
         LARGE_INTEGER ATP_CYCLES_TMP##name;\
         QueryPerformanceCounter(&ATP_CYCLES_TMP##name);\
         name##ATPTEST->info.test_run_cycles = ATP_CYCLES_TMP##name.QuadPart - ATP_CYCLES_##name.QuadPart;\
         name##ATPTEST->hits++
 
-    #else
-        #define ATP_START(name) \
+#else
+#define ATP_START(name) \
         uint64 ATP_CYCLES_##name = __rdtsc()
 
-        #define ATP_END(name) \
+#define ATP_END(name) \
         ATP_CYCLES_##name = __rdtsc() - ATP_CYCLES_##name;\
         *name##ATPTEST.info.test_run_cycles = ATP_CYCLES_##name;\
         *name##ATPTEST.hits++
-    
-    #endif
 
-        #define ATP_BLOCK(name) \
+#endif
+
+#define ATP_BLOCK(name) \
         ATP::ATP_SCOPED_TIMER_BLOCK ATP_SCOPED_TIMER_##name(name##ATPTEST)
 #else
-    #define ATP_REGISTER(name)              //Use to Register a TestType. Place in global scope.
-    #define ATP_EXTERN_REGISTER(name)       //Use to Call a TestType registered in a different compilation unit
-    #define ATP_START(name)                 //Use to start timer 
-    #define ATP_END(name)                   //Use to end timer and register timer info
-    #define ATP_BLOCK(name)                 //Scoped ATP_START() and ATP_END() block
+#define ATP_REGISTER(name)              //Use to Register a TestType. Place in global scope.
+#define ATP_EXTERN_REGISTER(name)       //Use to Call a TestType registered in a different compilation unit
+#define ATP_START(name)                 //Use to start timer 
+#define ATP_END(name)                   //Use to end timer and register timer info
+#define ATP_BLOCK(name)                 //Scoped ATP_START() and ATP_END() block
 #endif
 
 
