51d22a0c3e130023a12d2a2d85d740f1dac894b2
Adhavan <31651104+AdhavanT@users.noreply.github.com>
2020-12-04 23:36:32 +0530

    Added MultiTest support

--- atp.cpp
+++ atp.cpp
@@ -18,88 +18,99 @@
 #define ATP_ASSERT(X)
 #endif 
 
-#ifndef ATP_TURN_OFF
 
 namespace ATP
 {
+    //Singleton that is initialized on static initialization
+    TestType_Registry* testtype_registry;
+    static int32 registry_capacity;
 
-    struct TestType_DBuffer
+    TestType* add_multi_testtype_to_registry(const char* name_, uint32 buffer_size_)
     {
-        int32 length;
-        int32 capacity;
-        TestType* front = (TestType*)0;
-
-        TestType* add(TestType& new_member)
+        if (testtype_registry == (TestType_Registry*)0)
         {
-            length++;
-            if (length > capacity)
-            {
-                capacity = capacity + ATP_TESTTYPE_BUFFER_OVERFLOW_ADDON;
-                TestType* temp = (TestType*)ATP_REALLOC(front, capacity * sizeof(TestType));
-                ATP_ASSERT(temp);
-                front = temp;
-            }
-
-            TestType* temp = front;
-            temp = temp + (length - 1);
-            *temp = new_member;
-            return temp;
+            testtype_registry = (TestType_Registry*)ATP_CALLOC(1, sizeof(TestType_Registry));      //creating the global_testtypes buffer. pointer is initially zero initialized.
+        }
+        if (testtype_registry->front == (TestType*)0) //If buffer is not initilized yet
+        {
+            testtype_registry->no_of_testtypes = 0;
+            registry_capacity = ATP_TESTTYPE_BUFFER_INIT_CAPACITY;
+            testtype_registry->front = (TestType*)ATP_CALLOC(registry_capacity, sizeof(TestType));
+        }
+        //making room for new testtype in registry
+        testtype_registry->no_of_testtypes++;
+        if (testtype_registry->no_of_testtypes > registry_capacity)
+        {
+            registry_capacity = registry_capacity + ATP_TESTTYPE_BUFFER_OVERFLOW_ADDON;
+            TestType* temp = (TestType*)ATP_REALLOC(testtype_registry->front, registry_capacity * sizeof(TestType));
+            ATP_ASSERT(temp);   //couldn't realloc
+            testtype_registry->front = temp;
         }
 
-        void clear_buffer()
+        TestType new_testtype = {};
+        new_testtype.type = TestTypeFormat::MULTI;
+        new_testtype.name = name_;
+        if (buffer_size_ != 0)  //default initilize with specified size
         {
-            length = 0;
-            ATP_FREE(front);
+            new_testtype.tests.finished_tests = 0;
+            new_testtype.tests.size = buffer_size_;
+            new_testtype.tests.front = (TestInfo*)ATP_CALLOC(buffer_size_, sizeof(TestInfo));
         }
-
-        inline TestType& at(int32 index)
-        {
-            ATP_ASSERT(index >= 0 && index < length);
-            return (front[index]);
-        }
-    };
-
-    //NOTE: This is a pointer that is dynamically allocated cause it'll be zeroed out on dynamic initillization
-    //that happens before main and after all testtypes register themselves if it's a normal global variable
-    TestType_DBuffer* global_testtypes;
-
-    TestType* register_testtype(const char* name_)
-    {
-        if (global_testtypes == (TestType_DBuffer*)0)
-        {
-            global_testtypes = (TestType_DBuffer*)ATP_CALLOC(1, sizeof(TestType_DBuffer));      //creating the global_testtypes buffer to avoid zero-initilization from Ordered dynamic initialization
-        }
-        TestType test_type;
-        test_type.hits = 0;
-        test_type.info = { 0 };
-        test_type.name = name_;
-        if (global_testtypes->front == (TestType*)0) //If buffer is not initilized yet
-        {
-            global_testtypes->length = 0;
-            global_testtypes->capacity = ATP_TESTTYPE_BUFFER_INIT_CAPACITY;
-            global_testtypes->front = (TestType*)ATP_CALLOC(global_testtypes->capacity, sizeof(TestType));
-        }
-        return global_testtypes->add(test_type);
+        TestType* temp = testtype_registry->front;
+        temp = temp + (testtype_registry->no_of_testtypes - 1);
+        *temp = new_testtype;
+        return temp;
     }
 
 
-    TestType* get_testtype_registry(int32& no_of_testtypes)
+    //for SINGULAR testtypes
+    TestType* add_singular_testtype_to_registry(const char* name_)
     {
-        no_of_testtypes = global_testtypes->length;
-        return global_testtypes->front;
+        if (testtype_registry == (TestType_Registry*)0)
+        {
+            testtype_registry = (TestType_Registry*)ATP_CALLOC(1, sizeof(TestType_Registry));      //creating the global_testtypes buffer. pointer is initially zero initialized.
+        }
+        if (testtype_registry->front == (TestType*)0) //If buffer is not initilized yet
+        {
+            testtype_registry->no_of_testtypes = 0;
+            registry_capacity = ATP_TESTTYPE_BUFFER_INIT_CAPACITY;
+            testtype_registry->front = (TestType*)ATP_CALLOC(registry_capacity, sizeof(TestType));
+        }
+        //making room for new testtype in registry
+        testtype_registry->no_of_testtypes++;
+        if (testtype_registry->no_of_testtypes > registry_capacity)
+        {
+            registry_capacity = registry_capacity + ATP_TESTTYPE_BUFFER_OVERFLOW_ADDON;
+            TestType* temp = (TestType*)ATP_REALLOC(testtype_registry->front, registry_capacity * sizeof(TestType));
+            ATP_ASSERT(temp);   //couldn't realloc 
+            testtype_registry->front = temp;
+        }
+
+        TestType new_testtype = {};
+        new_testtype.type = TestTypeFormat::SINGULAR;
+        new_testtype.info = { 0 };
+        new_testtype.name = name_;
+
+        TestType* temp = testtype_registry->front;
+        temp = temp + (testtype_registry->no_of_testtypes - 1);
+        *temp = new_testtype;
+        return temp;
     }
 
     TestType* lookup_testtype(const char* name)
     {
-        for (int i = 0; i < global_testtypes->length; i++)
+        TestType *tt = testtype_registry->front;
+        for (int i = 0; i < testtype_registry->no_of_testtypes; i++)
         {
-            if (name == global_testtypes->at(i).name)
+            if (name == tt->name)
             {
-                return &global_testtypes->at(i);
+                return tt;
             }
+            tt++;
         }
         return 0;
     }
+
     f64 get_ms_from_test(TestType& test)
     {
         LARGE_INTEGER frequency;
@@ -109,21 +120,3 @@ namespace ATP
         return time_elapsed;
     }
 }
-#endif
-
-#ifdef ATP_TURN_OFF
-namespace ATP
-{
-
-    TestType* lookup_testtype(const char* name)
-    {
-        return nullptr;
-    }
-
-    f64 get_ms_from_test(TestType& test)
-    {
-        return 0;
-    }
-}
-
-#endif
--- atp.h
+++ atp.h
@@ -2,17 +2,19 @@
 #define ATP_HEADER_IS_INCLUDED
 
 /*
-TODO for anyone: This Library doesn't use any C++ features (mostly just syntax) and can easily be ported to C.
-
+-------------------------------<ATProfiler>--------------------------------------------------------
                  For more info -> github.com/AdhavanT/ATProfiler
 
-Consider adding the "atp_config.atp" file to get the description of and configure macros
+add the "atp_config.atp" file to configure macros
 
-ATP is a simple-to-use profiler built for games and other real-time applications.
+ATP is a simple-to-use profiler built for games and other applications.
 ATP focuses on low performance overhead and being easy to use.
 The trade-off is building the various "TestType" buffers at program start-up.
-ATP builds a global buffer to hold all "TestType"s
- Example:
+ATP builds a global buffer to hold all "TestType"s. 
+ATP does not automatically clean up this global testtype registery or "MULTI" type testtypes. 
+If concerned with cleanup, do manually by getting registry with get_testtype_registry().
+
+Usage Example:
 
         REGISTER_TEST(timer_name);    //This must be in global scope
 
@@ -32,32 +34,26 @@ ATP builds a global buffer to hold all "TestType"s
             ATP_BLOCK(timer_name);   //Creates a simple object whose destructor calls ATP_END() at scope end.
             ..do stuff i guess...
         }
-
+TODO for anyone: This Library doesn't use many C++ features (mostly just syntax) and can be easily be ported to C.
+-------------------------------</ATProfiler>-------------------------------------------------------
 */
 #include "atp_config.atp"
 
-
-#ifdef ATP_ONLY_ON_DEBUG
-#ifndef _DEBUG
-#define ATP_TURN_OFF
-#endif
-#endif
-
-
-
 //Dependencies 
 
-//Used for queryperformancecounter or rdtsc if on windows
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN     
-#include <Windows.h>
-#endif
+
 
 
 namespace ATP
 {
+    enum TestTypeFormat
+    {
+        SINGULAR, MULTI
+    };
 
+    typedef int b32;
     typedef int  int32;
+    typedef unsigned int uint32;
     typedef long long int64;
     typedef unsigned long long uint64;
     typedef float f32;
@@ -68,120 +64,232 @@ namespace ATP
         uint64 test_run_cycles;
     };
 
-    struct TestType
+    struct MultiTest_Buffer    //meant for MULTI testtypes. A buffer of TestInfos. 
     {
-        const char* name;
-        uint64 hits;
-        TestInfo info;
+        uint32 size;    //the amount of testinfos the buffer can hold
+        uint32 finished_tests;  
+        TestInfo* front = 0;
     };
 
+    struct TestType
+    {
+        TestTypeFormat type;
+        const char* name;
+        union       //A discrimated union used for both SINGULAR and MULTI testtypes
+        {
+            //SingularTestInfo
+            TestInfo info;
+
+            //MultiTestInfo (a buffer of TestInfos)
+            MultiTest_Buffer tests;
+
+        };
+    };
+
+    //singleton buffer that holds all TestTypes 
+    struct TestType_Registry
+    {
+        int32 no_of_testtypes;
+        TestType* front = (TestType*)0;
+    };
 }
 namespace ATP
 {
+    //pointer to TestType_Registry singleton
+    extern TestType_Registry* testtype_registry;
 
-    TestType* get_testtype_registry(int32& no_of_testtypes);
-
-    //that allows easy flushing of memory if not ATP_RECORD_ALL
+    //looks up a testtype from name. 
     TestType* lookup_testtype(const char* name);
 
-    //TODO: Agian, do this properly
+    //NOTE: this is ad-hoc. don't use for high performance.
     f64 get_ms_from_test(TestType& test);
 
-    TestType* register_testtype(const char* name_);
+    TestType* add_singular_testtype_to_registry(const char* name_);
+
+    TestType* add_multi_testtype_to_registry(const char* name_, uint32 buffer_size_);
 }
 
-#ifndef ATP_TURN_OFF
+
+//Defining macros :
+
+//For SINGULAR testtypes
+// ATP_REGISTER(name)              //Use to Register a TestType. Place in global scope.
+// ATP_EXTERN_REGISTER(name)       //Use to Call a TestType registered in a different compilation unit
+// ATP_START(name)                 //Use to start test 
+// ATP_END(name)                   //Use to end test and register test info
+// ATP_BLOCK(name)                 //A scoped test. Automatically calls ATP_START and calles ATP_END at end of scope.
+
+//For MULTI testtypes 
+// ATP_REGISTER_M(name, default_size)            //Use to Register a TestType. Place in global scope. 
+// ATP_EXTERN_REGISTER(name)                     //Use to Call a TestType registered in a different compilation unit
+// ATP_START_M(name)                             //Use to start test (same as ATP_START())
+// ATP_END_M(name, index)                        //Use to end timer and register testinfo of index test
+// ATP_BLOCK_M(name, index)                      //Scoped ATP_START() and ATP_END() block
+
+
+//-----------------------<Windows>-------------------------------------
+#ifdef _WIN32
+
+#define WIN32_LEAN_AND_MEAN     
+#include <Windows.h>
+#undef WIN32_LEAN_AND_MEAN
+
+//---------------------<SINGULAR TESTTYPE>-----------------------------
+//Use to Register a SINGULAR TestType. Place in global scope.
+#define ATP_REGISTER(name) \
+     ATP::TestType *ATPTESTTYPE_##name= ATP::add_singular_testtype_to_registry(#name)
+
+//Used to call to a test registered in a different compilation unit 
+#define ATP_EXTERN_REGISTER(name) \
+     extern ATP::TestType *ATPTESTTYPE_##name;
+
+#ifdef ATP_USE_QPC
+//Use to start test (using qpc to time)
+#define ATP_START(name) \
+        LARGE_INTEGER ATP_START_CYCLES_##name;\
+        QueryPerformanceCounter(&ATP_START_CYCLES_##name)
+
+//Use to end test
+#define ATP_END(name) \
+        LARGE_INTEGER ATP_END_CYCLES_##name;\
+        QueryPerformanceCounter(&ATP_END_CYCLES_##name);\
+        ATPTESTTYPE_##name->info.test_run_cycles = (ATP_END_CYCLES_##name.QuadPart - ATP_START_CYCLES_##name.QuadPart)\
 
 namespace ATP
 {
-#ifdef _WIN32 
-
-#ifdef ATP_USE_QPC
-
-    struct ATP_SCOPED_TIMER_BLOCK
+    struct ATP_SCOPED_TEST_BLOCK
     {
         TestType* testtype;
         LARGE_INTEGER cycles;
-        ATP_SCOPED_TIMER_BLOCK(TestType* tt)
+        ATP_SCOPED_TEST_BLOCK(TestType* tt)
             :testtype(tt)
         {
             QueryPerformanceCounter(&cycles);
         }
-        ~ATP_SCOPED_TIMER_BLOCK()
+        ~ATP_SCOPED_TEST_BLOCK()
         {
             LARGE_INTEGER tmp;
             QueryPerformanceCounter(&tmp);
             testtype->info.test_run_cycles = tmp.QuadPart - cycles.QuadPart;
-            testtype->hits++;
         }
     };
-#else 
-    struct ATP_SCOPED_TIMER_BLOCK
+}
+
+#else   //!ATP_USE_QPC
+//Use to start test (using rdtsc to time)
+#define ATP_START(name) \
+        uint64 ATP_START_CYCLES_##name = __rdtsc()
+
+//Use to end test
+#define ATP_END(name) \
+        ATP_END_CYCLES_##name = __rdtsc() - ATP_START_CYCLES_##name;\
+        ATPTESTTYPE_##name->info.test_run_cycles = ATP_END_CYCLES_##name
+
+
+namespace ATP
+{
+    struct ATP_SCOPED_TEST_BLOCK
     {
         TestType* testtype;
         uint64 cycles;
-        ATP_SCOPED_TIMER_BLOCK(TestType* tt)
+        ATP_SCOPED_TEST_BLOCK(TestType* tt)
             :testtype(tt)
         {
             cycles = __rdtsc();
         }
-        ~ATP_SCOPED_TIMER_BLOCK()
+        ~ATP_SCOPED_TEST_BLOCK()
         {
             testtype->info.test_run_cycles = __rdtsc() - cycles;
             testtype->hits++;
         }
     };
-#endif
-#endif
 }
 
-#define ATP_REGISTER(name) \
-     ATP::TestType *name##ATPTEST = ATP::register_testtype(#name)
+#endif
+//A scoped test. Automatically calls ATP_START and calles ATP_END at end of scope.
+#define ATP_BLOCK(name) \
+        ATP::ATP_SCOPED_TEST_BLOCK ATP_SCOPED_TEST_##name(ATPTESTTYPE_##name)
+//---------------------</SINGULAR TESTTYPE>----------------------------
 
-//Used to call to a test registered in a different compilation unit 
-#define ATP_EXTERN_REGISTER(name) \
-     extern ATP::TestType *name##ATPTEST;
+//---------------------<MULTI TESTTYPE>--------------------------------
+//For registering MULTI testtype. Set default_size to 0 to avoid automatic initialization.
+#define ATP_REGISTER_M(name, default_size) \
+     ATP::TestType *ATPTESTTYPE_##name = ATP::add_multi_testtype_to_registry(#name, default_size)
+
+//Use to start test. Same as ATP_START()
+#define ATP_START_M(name) ATP_START(name)
 
 #ifdef ATP_USE_QPC
-#define ATP_START(name) \
-        LARGE_INTEGER ATP_CYCLES_##name;\
-        QueryPerformanceCounter(&ATP_CYCLES_##name)
+//Use to end test. Include with index of MULTI testtype to add new testinfo to.
+#define ATP_END_M(name, index) \
+        LARGE_INTEGER ATP_END_CYCLES_##name;\
+        QueryPerformanceCounter(&ATP_END_CYCLES_##name);\
+        ATPTESTTYPE_##name->tests.front[(index)].test_run_cycles = (ATP_END_CYCLES_##name.QuadPart - ATP_START_CYCLES_##name.QuadPart);\
+        ATPTESTTYPE_##name->tests.finished_tests++
 
-#define ATP_END(name) \
-        LARGE_INTEGER ATP_CYCLES_TMP##name;\
-        QueryPerformanceCounter(&ATP_CYCLES_TMP##name);\
-        name##ATPTEST->info.test_run_cycles = ATP_CYCLES_TMP##name.QuadPart - ATP_CYCLES_##name.QuadPart;\
-        name##ATPTEST->hits++
+namespace ATP
+{
+    struct ATP_SCOPED_MULTITEST_BLOCK
+    {
+        TestType* testtype;
+        LARGE_INTEGER start_cycles;
+        uint32 index;
+        ATP_SCOPED_MULTITEST_BLOCK(TestType* tt, uint32 index_)
+            :testtype(tt), index(index_)
+        {
+            QueryPerformanceCounter(&start_cycles);
+        }
+        ~ATP_SCOPED_MULTITEST_BLOCK()
+        {
+            LARGE_INTEGER end;
+            QueryPerformanceCounter(&end);
+            testtype->tests.front[index].test_run_cycles = end.QuadPart - start_cycles.QuadPart;
+            testtype->tests.finished_tests++;
+        }
+    };
+}
 
 #else
-#define ATP_START(name) \
-        uint64 ATP_CYCLES_##name = __rdtsc()
+//Use to end test. Include with index of MULTI testtype to add new testinfo to.
+#define ATP_END_M(name, index) \
+        ATP_END_CYCLES_##name = __rdtsc() - ATP_START_CYCLES_##name;\
+        ATPTESTTYPE_##name->tests.front[(index)].test_run_cycles = ATP_END_CYCLES_##name;\
+        ATPTESTTYPE_##name->tests.finished_tests++
 
-#define ATP_END(name) \
-        ATP_CYCLES_##name = __rdtsc() - ATP_CYCLES_##name;\
-        *name##ATPTEST.info.test_run_cycles = ATP_CYCLES_##name;\
-        *name##ATPTEST.hits++
-
+namespace ATP
+{
+    struct ATP_SCOPED_MULTITEST_BLOCK
+    {
+        TestType* testtype;
+        uint64 start_cycles;
+        uint32 index;
+        ATP_SCOPED_MULTITEST_BLOCK(TestType* tt, uint32 index_)
+            :testtype(tt), index(index_)
+        {
+            start_cycles = __rdtsc();
+        }
+        ~ATP_SCOPED_MULTITEST_BLOCK()
+        {
+            uint64 end;
+            end = __rdtsc() - start_cycles;
+            testtype->tests.front[index].test_run_cycles = end;
+            testtype->tests.finished_tests++;
+        }
+    };
+}
 #endif
+//A scoped MULTI test. Automatically calls ATP_START and calles ATP_END at end of scope.
+#define ATP_BLOCK_M(name, index) \
+        ATP::ATP_SCOPED_MULTITEST_BLOCK ATP_SCOPED_TEST_##name(ATPTESTTYPE_##name, (index))
+//---------------------</MULTI TESTTYPE>-------------------------------
 
-#define ATP_BLOCK(name) \
-        ATP::ATP_SCOPED_TIMER_BLOCK ATP_SCOPED_TIMER_##name(name##ATPTEST)
-#else
-#define ATP_REGISTER(name)              //Use to Register a TestType. Place in global scope.
-#define ATP_EXTERN_REGISTER(name)       //Use to Call a TestType registered in a different compilation unit
-#define ATP_START(name)                 //Use to start timer 
-#define ATP_END(name)                   //Use to end timer and register timer info
-#define ATP_BLOCK(name)                 //Scoped ATP_START() and ATP_END() block
-#endif
+#endif // !Win32
+//-----------------------</Windows>-------------------------------------
 
 
-//Removing libraries config macros 
+//Removing atp_config macros 
 #define ATP_CONFIG_UNDEF
 #include "atp_config.atp"
 #undef ATP_CONFIG_UNDEF
 
-#ifdef _WIN32
-#undef WIN32_LEAN_AND_MEAN
-#undef ATP_USE_QPC
-#endif
 #endif // !ATP_HEADER_IS_INCLUDED
